04d35b9e726d3b962b1207f2b550873f51f0c113
jchyczynski <jasiek.ch16@gmail.com>
2021-12-11 17:21:51 +0100

    max altitude difference apogeum finder implementation

--- spiejs_systiemo/apogeum_finder.h
+++ spiejs_systiemo/apogeum_finder.h
@@ -6,39 +6,68 @@
 
 class Apogeum_finder
 {
-  altitude_t prev_alt = 0, curr_alt = 0; //TO DO
-  bool reached_apogeum = false;
-  int neg_delta_cnt = 0;
-  public:
+    altitude_t prev_alt = 0, curr_alt = 0, max_alt; //TO DO
+    bool reached_apogeum = false;
+    int neg_delta_cnt = 0;
+public:
 
-  void insertAltitude(altitude_t altitude)
-  {
-    prev_alt = curr_alt;
-    curr_alt = altitude;
-
-    altitude_t delta = curr_alt - prev_alt;
-
-    if(delta < 0)
+    void insertAltitude(altitude_t altitude)
     {
-        neg_delta_cnt++;
-    }
-    else
-    {
-        neg_delta_cnt = 0;
-    }
+        update_altitude(altitude);
 
-    if(neg_delta_cnt >= NEG_STREAK_REQ)
-    {
-        reached_apogeum = true;
+        update_neg_streak();
+        update_max_alt();
+
+        if (reached_apogeum_by_neg_streak() &&
+                reached_apogeum_by_max_alt())
+        {
+            reached_apogeum = true;
+        }
+
     }
 
-  }
+    bool get_reached_apogeum()
+    {
+        return reached_apogeum;
+    }
 
-  bool get_reached_apogeum()
-  {
-    return reached_apogeum;
-  }
-  
+private:
+    void update_altitude(altitude_t altitude)
+    {
+        prev_alt = curr_alt;
+        curr_alt = altitude;
+    }
+
+    void update_neg_streak()
+    {
+        altitude_t delta = curr_alt - prev_alt;
+
+        if (delta < 0)
+        {
+            neg_delta_cnt++;
+        } else
+        {
+            neg_delta_cnt = 0;
+        }
+    }
+
+    bool reached_apogeum_by_neg_streak()
+    {
+        return neg_delta_cnt >= NEG_STREAK_REQ;
+    }
+
+    void update_max_alt()
+    {
+        if(curr_alt > max_alt)
+        {
+            max_alt = curr_alt;
+        }
+    }
+
+    bool reached_apogeum_by_max_alt()
+    {
+        return max_alt - curr_alt > MAX_ALT_DIFF_REQ;
+    }
 };
 
 #endif
--- spiejs_systiemo/params.h
+++ spiejs_systiemo/params.h
@@ -1,9 +1,10 @@
 #ifndef PARAMS
 #define PARAMS
 
-int NEG_STREAK_REQ = 5;
-
-typedef int altitude_t;
+typedef double altitude_t;
 typedef int my_time_t;
+
+int NEG_STREAK_REQ = 5;
+altitude_t MAX_ALT_DIFF_REQ = 1; //meter
 
 #endif
